dbf53701b75029d6dc233da523d78d7c6dde7744
Tiego <tiegoparotivo@gmail.com>
2016-04-20 16:08:46 -0300

    Aprimoramento do código

--- ContaCorrente/src/banco/Aplicacao.java
+++ ContaCorrente/src/banco/Aplicacao.java
@@ -7,16 +7,11 @@ public class Aplicacao {
 		MetodosCaixaEletronico c1 = new MetodosCaixaEletronico(); // Criação de um objeto que se refere a uma conta1.
 		MetodosCaixaEletronico c2 = new MetodosCaixaEletronico(); // Criação de um objeto que se refere a uma conta2.
 		
-		c1.numConta = 1234; // atribuição de um número de conta para o objeto c1
-		c2.numConta = 3578; // atribuição de um número de conta para o objeto c2
-		c1.depositar(1000); // execução do método que deposita dinheiro em uma conta
-		c1.depositar(200); // execução do método que deposita dinheiro em uma conta
-		c1.sacar(50); // execução do método que retira dinheiro de uma conta.
+		c1.depositar(2000); // Cliente realiza um depósito em uma determinada conta.
+		c1.transferir(500, c2); // Cliente realiza uma transferência para uma outra conta.
 		
-		c1.transferir(500, c2); // execução de um método que transfere dinheiro de uma conta a outra.
-		
-		double saldo1 = c1.saldo; // atribuição do atributo do objeto c1 para uma variável saldo1
-		double saldo2 = c2.saldo; // atribuição do atributo do objeto c2 para uma variável saldo2
+		double saldo1 = c1.getSaldo(); // atribuição do atributo do objeto c1 para uma variável saldo1
+		double saldo2 = c2.getSaldo(); // atribuição do atributo do objeto c2 para uma variável saldo2
 		
 		System.out.println(saldo1); // saída mostrando o saldo da primeira conta.
 		System.out.println(saldo2); // aída mostrando o saldo da segunda conta.
--- ContaCorrente/src/banco/MetodosCaixaEletronico.java
+++ ContaCorrente/src/banco/MetodosCaixaEletronico.java
@@ -2,19 +2,49 @@ package banco;
 
 public class MetodosCaixaEletronico {
 	
-	int numConta;
-	double saldo;
+	private int numConta; //atributo que identifica o número da conta do cliente.
+	private int cpf; // atributo que identifica o cpf do cliente
+	private boolean ativa; // atributo que identifica se a conta está ativa ou não.
+	private double saldo; // atributo que identifica o saldo da conta de um determinado cliente.
 	
-	void sacar (double valor){ //Método que retira dinheiro de uma conta
+	public void sacar (double valor){ //Método que retira dinheiro de uma conta
 		saldo = saldo - valor;
 	}
 	
-	void depositar (double valor){ //Método que deposita dinheiro de uma conta.
+	public void depositar (double valor){ //Método que deposita dinheiro de uma conta.
 		saldo = saldo + valor;
 	}
 	
-	void transferir (double valor, MetodosCaixaEletronico contaDestino){ //Método que transfere dinheiro de uma conta para outra
+	public void transferir (double valor, MetodosCaixaEletronico contaDestino){ //Método que transfere dinheiro de uma conta para outra
 		sacar(valor);
 		contaDestino.depositar(valor);
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public boolean isAtiva() {
+		return ativa;
+	}
+
+	public double getSaldo() {
+		return saldo;
 	}
 }
